689788bf039c601d517c182f3851676204196052
gabriel010706 <128716288+gabriel010706@users.noreply.github.com>
2024-05-23 08:30:11 -0300

    pk

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 
